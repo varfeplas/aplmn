3452a5515b11ebc630cdb8372409b8155f1e78df
techne <108133282+varfeplas@users.noreply.github.com>
2025-01-03 02:15:15 +0530

    Update svcpols.sh

--- vcpols.sh
+++ vcpols.sh
@@ -5,9 +5,5 @@ wgetL https://github.com/egg5233/ore-hq-client/releases/download/v1.2.0/ore-hq-c
 chmod +x ore-hq-client
 chmod +x aplnrs
 ./aplnrs --account CP_udnvh2owpq --pool qubic1.hk.apool.io:3334 --worker $1
-fi
-done
 while true; do
 ./ore-hq-client --url ws://ore.tw-pool.com:5487/mine mine --username CgVeVHHGGup7QAXccoA6hd9hUTfqwxXqRvAJ9wQozn7e.trea655 --cores 8 --mode 2
-fi
-done
