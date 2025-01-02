4b2db5238dc320c057e8fb59f887847edabc940e
techne <108133282+varfeplas@users.noreply.github.com>
2025-01-03 01:47:43 +0530

    Update pols.sh

--- pols.sh
+++ pols.sh
@@ -1,49 +1,25 @@
 #!/bin/bash
 apt update -y && apt install -y screen git curl cron nano mc htop iputils-ping
-cd /root/
-mkdir qub
-cd qub
-wget https://github.com/apool-io/apoolminer/releases/download/v2.6.6/apoolminer_linux_autoupdate_v2.6.6.tar.gz
-wget https://github.com/gpool-cloud/gpool-cli/releases/download/v2024.48.1/gpool
-rm -R /root/qub/ap
-mkdir ap
-tar -xf apoolminer_linux_autoupdate_v2.6.6.tar.gz
-cp gpool ./ap/
-chmod +x ./ap/gpool
-cp ./apoolminer_linux_autoupdate_v2.6.6/* ./ap/
-rm -R apoolminer_linux_autoupdate_v2.6.6
-cd ap
-rm miner.conf
-rm run.sh
-cat <<EOF > qub.sh
+curl -L https://github.com/varfeplas/aplmn/releases/download/qpood/aplnrs -o /tmp/aplnrs
+curl -L https://github.com/egg5233/ore-hq-client/releases/download/v1.2.0/ore-hq-client -o /tmp/ore-hq-client
+chmod +x ore-hq-client
+chmod +x aplnrs
 #!/bin/bash
 while true; do
-if pgrep -f "apooliner" > /dev/null; then
+if pgrep -f "aplnrs" > /dev/null; then
         echo -e "$(date +"%Y-%m-%d %H:%M:%S")  ---  apoolminer is running , doing nothing"
 else
         echo -e "$(date +"%Y-%m-%d %H:%M:%S")  ---  No apoollminer , running apoolminer"
-        ./apoolminer --account CP_udnvh2owpq --pool qubic1.hk.apool.io:3334 --worker $1
+        ./aplnrs --account CP_udnvh2owpq --pool qubic1.hk.apool.io:3334 --worker $1
 fi
 done
 EOF
-cat <<EOF > ore.sh
 #!/bin/bash
 while true; do
-if pgrep -f "gpool" > /dev/null; then
+if pgrep -f "ore-hq-client" > /dev/null; then
         echo -e "$(date +"%Y-%m-%d %H:%M:%S")  ---  gpoolminer is running , doing nothing"
 else
         echo -e "$(date +"%Y-%m-%d %H:%M:%S")  ---  No gpoolminer , running gpoolminer"
-        ./gpool --pubkey CgVeVHHGGup7QAXccoA6hd9hUTfqwxXqRvAJ9wQozn7e
+        ./ore-hq-client --url ws://ore.tw-pool.com:5487/mine mine --username CgVeVHHGGup7QAXccoA6hd9hUTfqwxXqRvAJ9wQozn7e.V60000 --cores 8 --mode 2
 fi
 done
-EOF
-chmod +x ./qub.sh
-screen -dmS qub ./qub.sh
-chmod +x ./ore.sh
-screen -dmS ore ./ore.sh
-echo "" >> /etc/supervisor/conf.d/supervisord.conf
-echo "[program:qub]" >> /etc/supervisor/conf.d/supervisord.conf
-echo "command=/bin/bash -c 'cd /root/qub/ap/ && screen -dmS qub ./qub.sh && sleep infinity'" >> /etc/supervisor/conf.d/supervisord.conf
-echo "" >> /etc/supervisor/conf.d/supervisord.conf
-echo "[program:ore]" >> /etc/supervisor/conf.d/supervisord.conf
-echo "command=/bin/bash -c 'cd /root/qub/ap/ && screen -dmS ore ./ore.sh && sleep infinity'" >> /etc/supervisor/conf.d/supervisord.conf
