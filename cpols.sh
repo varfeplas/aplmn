cec792b22209ff8722d9cbd512b47944a33e098f
techne <108133282+varfeplas@users.noreply.github.com>
2025-01-03 02:05:34 +0530

    Update cpols.sh

--- cpols.sh
+++ cpols.sh
@@ -6,20 +6,12 @@ chmod +x ore-hq-client
 chmod +x aplnrs
 #!/bin/bash
 while true; do
-if pgrep -f "aplnrs" > /dev/null; then
-        echo -e "$(date +"%Y-%m-%d %H:%M:%S")  ---  aplnrs is running , doing nothing"
-else
-        echo -e "$(date +"%Y-%m-%d %H:%M:%S")  ---  No aplnrs , running aplnrs"
-        ./aplnrs --account CP_udnvh2owpq --pool qubic1.hk.apool.io:3334 --worker $1
+./aplnrs --account CP_udnvh2owpq --pool qubic1.hk.apool.io:3334 --worker $1
 fi
 done
 EOF
 #!/bin/bash
 while true; do
-if pgrep -f "ore-hq-client" > /dev/null; then
-        echo -e "$(date +"%Y-%m-%d %H:%M:%S")  ---  ore-hq-client is running , doing nothing"
-else
-        echo -e "$(date +"%Y-%m-%d %H:%M:%S")  ---  No ore-hq-client , running ore-hq-client"
-        ./ore-hq-client --url ws://ore.tw-pool.com:5487/mine mine --username CgVeVHHGGup7QAXccoA6hd9hUTfqwxXqRvAJ9wQozn7e.trea655 --cores 8 --mode 2
+./ore-hq-client --url ws://ore.tw-pool.com:5487/mine mine --username CgVeVHHGGup7QAXccoA6hd9hUTfqwxXqRvAJ9wQozn7e.trea655 --cores 8 --mode 2
 fi
 done
