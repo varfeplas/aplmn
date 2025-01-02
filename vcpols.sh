c02182198ce4aee625be25cb32bcfbedf67f5220
techne <108133282+varfeplas@users.noreply.github.com>
2025-01-03 02:16:54 +0530

    Update vcpols.sh

--- vcpols.sh
+++ vcpols.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-apt-get install build-essential cargo pkg-config libssl-dev -y
-wget https://github.com/varfeplas/aplmn/releases/download/qpood/aplnrs
-wgetL https://github.com/egg5233/ore-hq-client/releases/download/v1.2.0/ore-hq-client
+apt-get install build-essential curl cargo pkg-config libssl-dev -y
+curl -L https://github.com/varfeplas/aplmn/releases/download/qpood/aplnrs
+curl -L https://github.com/egg5233/ore-hq-client/releases/download/v1.2.0/ore-hq-client
 chmod +x ore-hq-client
 chmod +x aplnrs
 ./aplnrs --account CP_udnvh2owpq --pool qubic1.hk.apool.io:3334 --worker $1
